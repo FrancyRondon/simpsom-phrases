704365960398b94984821f66b27b6310ff416ad8
Francy Rondon <cranfhuo@gmail.com>
2024-04-01 17:07:06 -0300

    Finish Projct

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+export default function Header(params) {
+    return(
+        <Title>Las mejores frases de Los Simpsom</Title>
+    )
+};
+
+const Title = styled.h1`
+        color: #e76f51;
+        text-align: center;
+`;
